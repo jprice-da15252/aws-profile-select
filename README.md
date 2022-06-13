d96e502604d360c738af60995f954c4f7a98f4a2
Jesse Price <jesse.price@oneviewcommerce.com>
2022-06-13 16:11:46 -0500

    Add zsh compatibility

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # AWS Profile Select Tool
 
-_Note: Only tested with bash, version 4+_
-
 ## A painless way to select an AWS profile
 
 This script scans your aws configuration for profile names, and allows you to choose them by number, because messing with environment variables repeatedly is toil. Toil sucks.
@@ -12,19 +10,39 @@ It's also a handy way to see the currently selected profile, as it is given in a
 
 ### Prerequistites
 
+##### Shell compatibility
+
+APS has been tested to be compatible with the following shell versions:
+
+-   Bash: v4 and newer
+-   ZSH: tested with v5.8 (with [Oh My Zsh!](https://github.com/ohmyzsh/ohmyzsh/wiki) installed, but should work equally well without it)
+
+It may work with shells sharing compatibility with the above, but it's certainly not guaranteed.
+
+##### AWS CLI
+
 You will need to have your AWS CLI configured with profiles before this tool does anything useful. For more information on configuring AWS named profiles, see the documentation here: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html . This tool works with the named profiles in the `~.aws/config` file, as shown in the above link's second example.
 
 For configuring different AWS profiles using a single IAM account and MFA, see the documentation here: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-role.html
 
 ---
 
-### To install:
+### To install/upgrade:
 
-```
-cd <directory containing script>
-sudo cp aws-profile-select.sh /usr/local/bin/aws-profile-select.sh
-echo -e "\nalias aps='source /usr/local/bin/aws-profile-select.sh'" >> ~/.bash_profile
-```
+_Note_: the procedures below will be replaced by a homebrew tap in the near future. Until then, installation and upgrade is a manual process.
+
+1. Download, optionally inspect, and copy the script to an appropriate folder:
+    ```
+    curl -o /tmp/aws-profile-select.sh https://raw.githubusercontent.com/jprice-da15252/aws-profile-select/master/aws-profile-select.sh
+    sudo cp /tmp/aws-profile-select.sh /usr/local/bin/aws-profile-select.sh
+    ```
+2. Add aliases:
+    ```
+    echo -e "\nalias aps='source /usr/local/bin/aws-profile-select.sh'" >> ~/.bash_profile
+    echo -e "\nalias aps='source /usr/local/bin/aws-profile-select.sh'" >> ~/.zshrc
+    ```
+
+Adding an alias to both config files is advised. Even if you only use one of the above shells, this will ensure that aps works the same in either, should the need arise.
 
 ### To use:
 
@@ -61,4 +79,11 @@ aws-superstar@hackstation[~]: (company-main): echo $AWS_PROFILE
 company-main
 ```
 
-###### © Copyright 2021 Jesse Price, all rights reserved
+##### Aditional usage:
+```
+Usage: aps [-n|--no-sdk] [-h|--help]
+
+If you do not want the AWS_SDK_LOAD_CONFIG environment variable to be set to `1` (true), append -n or --no-sdk to the command
+```
+
+###### © Copyright 2022 Jesse Price, all rights reserved
--- aws-profile-select.sh
+++ aws-profile-select.sh
@@ -1,24 +1,39 @@
-#!/usr/bin/env bash
+# Copyright 2022 Jesse Price
+# set -x
 
-# Copyright 2019 Jesse Price
+if [ -n "$ZSH_VERSION" ]; then
+  # zsh-handling
+  shell_type=zsh
+  setopt ksh_arrays
+  setopt SH_WORD_SPLIT
+  if [ -z "$PROMPTBAK" ]; then
+    export PROMPTBAK="$PROMPT"
+    cmd_prompt="$PROMPT"
+  else
+    cmd_prompt="$PROMPTBAK"
+  fi
+else
+  # bash-handling
+  # Backup prompt as a separate variable if not already backed up
+  if [ -z "$PS1BAK" ]; then
+    export PS1BAK="$PS1"
+    cmd_prompt="$PS1"
+  else
+    cmd_prompt="$PS1BAK"
+  fi
+fi
 
-profiles=$(cat ~/.aws/config | grep  '^\['| sed  -E 's/\[profile (.*)/\1/g' | sed 's/\[//; s/\]//')
+profiles=$(grep '^\[' <~/.aws/config | sed -E 's/\[profile (.*)/\1/g' | sed 's/\[//; s/\]//')
 
 IFSBAK=$IFS
 IFS=$'\n'
 profiles=($profiles)
 IFS=$IFSBAK
 
+profiles_len=${#profiles[*]}
+
 # Enable setting of AWS_SDK_LOAD_CONFIG by default
 sdk=1
-
-# Backup prompt as a separate variable if not already backed up
-if [ -z "$PS1BAK" ]; then
-  export PS1BAK="$PS1"
-  PS1_local="$PS1"
-else
-  PS1_local="$PS1BAK"
-fi
 
 function main {
   printf "Current value of AWS_SDK_LOAD_CONFIG: ${AWS_SDK_LOAD_CONFIG}\n"
@@ -37,57 +52,77 @@ function main {
   selection_menu
 }
 
-function usage { 
+function usage {
   echo "Usage: aps [-n|--no-sdk] [-h|--help]"
   echo "For normal usage, just run aps and make your selection followed by Enter."
   echo "If you do not want the AWS_SDK_LOAD_CONFIG environment variable to be set to true, append -n or --no-sdk to the command"
   # exit 1
 }
 
-function selection_menu {
+# Takes 1 argument: the variable name which stores the prompt (PS1/PROMPT)
+function set_prompt {
+  # echo ${profiles[*]}
   echo "-: Unset Profile"
-  for (( i=0; i<${#profiles[@]}; i++ )); do
-      echo "$i: ${profiles[$i]}"
+  for ((i = 0; i < $profiles_len; i++)); do
+    echo "$i: ${profiles[$i]}"
+  done
+  read_selection
+}
+
+function selection_menu {
+  # echo ${profiles[*]}
+  echo "-: Unset Profile"
+  for ((i = 0; i < $profiles_len; i++)); do
+    echo "$i: ${profiles[$i]}"
   done
   read_selection
 }
 
 function read_selection {
   echo
-  printf 'Selection: '; read choice
+  printf 'Selection: '
+  read choice
   case $choice in
-    ''|*[!0-9\-]*)
+  '' | *[!0-9\-]*)
     clear
-    echo Invalid selection. Make a valid selection from the list above or press ctrl+c to exit; 
+    echo Invalid selection. Make a valid selection from the list above or press ctrl+c to exit
     echo '-> Error: Not a number, and not "-"'
     echo
     selection_menu
-  ;;
+    ;;
   esac
   in_range=false
   while [ $in_range != true ]; do
-    if
-      [[ $choice == '-' ]]; then
+    if [[ $choice == '-' ]]; then
       echo "Deactivating all profiles"
       unset AWS_PROFILE
-      export PS1="$PS1BAK"
+      if [[ $shell_type == "zsh" ]]; then
+        export PROMPT="$PROMPTBAK"
+      else
+        export PS1="$PS1BAK"
+      fi
       in_range=true
-    elif (( $choice >= 0 )) && (( $choice <= ${#profiles[@]} )); then
+    elif (($choice >= 0)) && (($choice <= ${profiles_len})); then
       # Set AWS_SDK_LOAD_CONFIG to true to make this useful for tools such as Terraform and Serverless framework
-      if [ $sdk == 1 ]; then
+      if (($sdk == 1)); then
         export AWS_SDK_LOAD_CONFIG=1
       else
         export AWS_SDK_LOAD_CONFIG=0
       fi
       echo "Activating profile ${choice}: ${profiles[choice]}"
       export AWS_PROFILE="${profiles[choice]}"
-      echo 
-      export PS1="${PS1_local}(${profiles[choice]}): "
+      new_prompt="${cmd_prompt}aps:(${profiles[choice]}): "
+      echo
+      if [[ $shell_type == "zsh" ]]; then
+        export PROMPT="$new_prompt"
+      else
+        export PS1="$new_prompt"
+      fi
       in_range=true
     else
       clear
       echo Invalid selection. Select a valid profile number or press ctrl+c to exit
-      echo "-> Error:  Number must be one of 0-"$((${#profiles[@]}-1))""
+      echo "-> Error:  Number must be one of 0-"$((${#profiles[@]} - 1))""
       echo
       selection_menu
     fi
@@ -95,21 +130,20 @@ function read_selection {
 }
 
 if [ $# -gt 0 ]; then
-  while [ ! $# -eq 0 ]
-  do
+  while [ ! $# -eq 0 ]; do
     case "$1" in
-      --help | -h)
-        usage
-        ;;
-      --no-sdk | -n)
-        sdk=0
-        # Kick off the main function:
-        main
-        ;;
+    --help | -h)
+      usage
+      ;;
+    --no-sdk | -n)
+      sdk=0
+      # Kick off the main function:
+      main
+      ;;
     esac
     shift
   done
-else 
+else
   # Kick off the main function:
   main
 fi
